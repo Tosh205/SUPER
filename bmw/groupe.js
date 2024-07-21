de3f11b45656cf24d0aa3865355c481ec8f158e1
Tosh205 <officialtosh13@gmail.com>
2024-07-21 09:32:02 +0300

    sup

--- bmw/groupe.js
+++ bmw/groupe.js
@@ -31,7 +31,7 @@ zokou({ nomCom: "tagall", categorie: 'Group', reaction: "📣" }, async (dest, z
   let membresGroupe = verifGroupe ? await infosGroupe.participants : ""
   var tag = ""; 
   tag += `========================\n  
-        🌟 *Baraka-Md* 🌟
+        🌟 *Super-Md* 🌟
 ========================\n
 👥 Group : ${nomGroupe} 🚀 
 👤 Autor : *${nomAuteurMessage}* 👋 
